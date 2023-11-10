2f0f8b1120c8aad824b793d2e3c97d58e59fb89d
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2023-11-10 17:11:56 -0300

    add exercicio for

---
+++ Ex02QuantidadeParesImpares.java
@@ -0,0 +1,30 @@
+package lacosRepeticao;
+
+import java.util.Scanner;
+
+public class Ex02QuantidadeParesImpares {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int i, num, pares = 0, impares = 0;
+
+		for (i = 1; i < 11; i++) {
+			System.out.print("Digite o " + i + "º" + "numero" + ":  ");
+			num = sc.nextInt();
+
+			if (num % 2 == 0) {
+				pares++;
+			} else {
+				impares++;
+			}
+		}
+
+		System.out.println("\nTotal de números pares: " + pares);
+		System.out.println("Total de números impares: " + impares);
+
+		sc.close();
+	}
+
+}
